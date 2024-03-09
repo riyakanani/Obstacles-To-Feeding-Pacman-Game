5eb4c3250fc842375cf24c66bd78a663c4247161
riyakanani <riyakanani03@gmail.com>
2024-03-09 13:40:21 -0500

    working code

--- sketch.js
+++ sketch.js
@@ -23,6 +23,8 @@ var optionButtonWidth = 20;
 var correctChoice = 0;
 var gotCorrect = -1;
 var q1, a1, a2, a3;
+var eatCount = 0;
+var ghostOn = true;
 
 function preload() {
   blueMonsterImage = loadImage("Blue Monster.png");
@@ -89,25 +91,67 @@ function setup() {
   orangeMonster2.velocityX = -3; 
 }
 
+function resetGhost(){
+  ghostOn = true;
+  blueMonster.x = 190;
+  blueMonster.y = 145;
+  orangeMonster.x = 190;
+  orangeMonster.y = 105;
+  pinkMonster.x = 190;
+  pinkMonster.y = 190;
+  redMonster.x = 190;
+  redMonster.y = 230;
+  orangeMonster2.x = 190;
+  orangeMonster2.y = 270
+  
+  blueMonster.velocityX = 3;
+  orangeMonster.velocityX = -3;
+  pinkMonster.velocityX = -3;
+  redMonster.velocityX = 3;
+  orangeMonster2.velocityX = -3; 
+}
+
+function stopGhost() {
+  ghostOn = false;
+  blueMonster.velocityX = 0;
+  orangeMonster.velocityX = 0;
+  pinkMonster.velocityX = 0;
+  redMonster.velocityX = 0;
+  orangeMonster2.velocityX = 0; 
+}
+
 function draw() {
-  background(backgroundImage);
+  background(0, 0, 0);
+  
+  if(lives == 0 || eatCount == 6){
+    mode = end;
+  } 
+  
   if (mode == welcome){
-    textSize(32);
+    textSize(28);
     textAlign(CENTER, CENTER);
-    text("welcome to pacman", 200, 200);
+    text("Obstacles to Feeding Pacman", 200, 200);
     textSize(16);
     text("use arrow keys to play", 200, 230);
     startButton.mouseClicked(startGame);
     
   } else if (mode == game){
-    startButton.remove();
+    if (ghostOn == false){
+      resetGhost();
+    }
     gameMode();
   } else if (mode == question){
-    questionMode()
-    if(gotCorrect == 1 || gotCorrect ==0){
+    if(ghostOn == true){
+      stopGhost();
+    }
+    questionMode();
+    if(gotCorrect == 1 || gotCorrect == 0){
       mode = game;
     }
+  } else if(mode == end){
+    endMode();
   }
+  
 }
 
 function gameMode(){
@@ -131,7 +175,202 @@ function gameMode(){
   p4.shapeColor = "green";
   p5.shapeColor = "blue";
   p6.shapeColor = "purple";
-  fill("black");
+  
+  
+  if (keyDown("left")) {
+    pacman.x = pacman.x - 3;
+  }
+
+  if (keyDown("right")) {
+    pacman.x = pacman.x + 3;
+  }
+
+  if (keyDown("down")) {
+    pacman.y = pacman.y + 3;
+  }
+
+  if (keyDown("up")) {
+    pacman.y = pacman.y - 3;
+  }
+  
+  if(pacman.isTouching(cherry)){
+    q1 = "The ______ is defined as the difference between actual yield and potential yield."
+    a1 = "Difference in Yield"
+    a2 = "Yield Loss"
+    a3 = "Yield Gap"
+    correctChoice = 3;
+    
+    if(gotCorrect == 1){
+      pacman.x = 40;
+      pacman.y = 370;
+      cherry.destroy();
+      score = score + 400;
+      gotCorrect = -1;
+      correctChoice = 0;
+      eatCount += 1;
+    } else if(gotCorrect == 0){
+      pacman.x = 40;
+      pacman.y = 370;
+      lives = lives - 1;
+      gotCorrect = -1;
+      correctChoice = 0;
+    }else{
+      mode = question;
+    }
+    
+  }
+
+  if(pacman.isTouching(p2)){
+    q1 = "Which of the following methods of saltwater intrusion management involve planting salt-tolerant crops and managing invasive species?"
+    a1 = "Retreat"
+    a2 = "Accommodate"
+    a3 = "Protect"
+    correctChoice = 2;
+    
+    if(gotCorrect == 1){
+      pacman.x = 40;
+      pacman.y = 370;
+      p2.destroy();
+      score = score + 20;
+      gotCorrect = -1;
+      correctChoice = 0;
+      eatCount += 1;
+    } else if(gotCorrect == 0){
+      pacman.x = 40;
+      pacman.y = 370;
+      lives = lives - 1;
+      gotCorrect = -1;
+      correctChoice = 0;
+    } else{
+      mode = question;
+    }
+  }
+  
+  if(pacman.isTouching(p3)){
+    q1 = "What is NOT one of Evan Fraser’s 4 strategies to feed 9.7B people?"
+    a1 = "Science and Technology"
+    a2 = "Intensification"
+    a3 = "Local Food Systems"
+    correctChoice = 2;
+    
+    if(gotCorrect == 1){
+      pacman.x = 40;
+      pacman.y = 370;
+      p3.destroy();
+      score = score + 20;
+      gotCorrect = -1;
+      correctChoice = 0;
+      eatCount += 1;
+    } else if(gotCorrect == 0){
+      pacman.x = 40;
+      pacman.y = 370;
+      lives = lives - 1;
+      gotCorrect = -1;
+      correctChoice = 0;
+    } else{
+      mode = question;
+    }
+  }
+  
+  if(pacman.isTouching(p4)){
+    q1 = "What percentage of freshwater is consumed by agriculture, domestic use, and industry, respectively?"
+    a1 = "Agriculture: 70%; Domestic Use: 10%; Industry: 20%"
+    a2 = "Agriculture: 70%; Domestic Use: 15%; Industry: 15%"
+    a3 = "Agriculture: 50%; Domestic Use: 15%; Industry: 35%"
+    correctChoice = 1;
+    
+    if(gotCorrect == 1){
+      pacman.x = 40;
+      pacman.y = 370;
+      p4.destroy();
+      score = score + 20;
+      gotCorrect = -1;
+      correctChoice = 0;
+      eatCount += 1;
+    } else if(gotCorrect == 0){
+      pacman.x = 40;
+      pacman.y = 370;
+      lives = lives - 1;
+      gotCorrect = -1;
+      correctChoice = 0;
+    } else{
+      mode = question;
+    }
+  }
+  
+  if(pacman.isTouching(p5)){
+    q1 = "What percentage of food is wasted in developed countries?"
+    a1 = "35%"
+    a2 = "50%"
+    a3 = "40%"
+    correctChoice = 3;
+    
+    if(gotCorrect == 1){
+      pacman.x = 40;
+      pacman.y = 370;
+      p5.destroy();
+      score = score + 20;
+      gotCorrect = -1;
+      correctChoice = 0;
+      eatCount += 1;
+    } else if(gotCorrect == 0){
+      pacman.x = 40;
+      pacman.y = 370;
+      lives = lives - 1;
+      gotCorrect = -1;
+      correctChoice = 0;
+    } else{
+      mode = question;
+    }
+  }
+  
+  if(pacman.isTouching(p6)){
+    q1 = "We currently produce enough food (calories) to feed the entire population."
+    a1 = "True"
+    a2 = "False"
+    a3 = "Wrong Answer"
+    correctChoice = 1;
+    
+    if(gotCorrect == 1){
+      pacman.x = 40;
+      pacman.y = 370;
+      p6.destroy();
+      score = score + 20;
+      gotCorrect = -1;
+      correctChoice = 0;
+      eatCount += 1;
+    } else if(gotCorrect == 0){
+      pacman.x = 40;
+      pacman.y = 370;
+      lives = lives - 1;
+      gotCorrect = -1;
+      correctChoice = 0;
+    } else{
+      mode = question;
+    }
+  }
+
+  if(pacman.isTouching(orangeMonster)||pacman.isTouching(blueMonster)||pacman.isTouching(orangeMonster2)||pacman.isTouching(pinkMonster)||pacman.isTouching(redMonster)){
+    lives = lives - 1;
+    pacman.x = 40;
+    pacman.y = 370;
+    
+    if(lives === 0){
+      textSize(30);
+      fill("black");
+      text("GAME OVER",300,200);
+      blueMonster.velocityX = 0;
+      orangeMonster.velocityX = 0;
+      pinkMonster.velocityX = 0;
+      redMonster.velocityX = 0;
+      orangeMonster2.velocityX = 0; 
+      pacman.destroy();
+      
+      
+    }
+
+  }
+  fill("white");
   textSize(12);
   text("score:"+ score,320,30);
   text("lives:"+ lives,80,30);
@@ -163,137 +402,30 @@ function gameMode(){
   pacman.bounceOff(w19);
   drawSprites();
   
-  
-  if (keyDown("left")) {
-    pacman.x = pacman.x - 3;
-  }
-
-  if (keyDown("right")) {
-    pacman.x = pacman.x + 3;
-  }
-
-  if (keyDown("down")) {
-    pacman.y = pacman.y + 3;
-  }
-
-  if (keyDown("up")) {
-    pacman.y = pacman.y - 3;
-  }
-  
-  if(pacman.isTouching(cherry)){
-    q1 = "lefh ewfn efih"
-    a1 = "kwefb"
-    a2 = "wkejn"
-    a3 = "kwefl"
-    correctChoice = 1;
-    
-    if(gotCorrect == 1){
-      cherry.destroy();
-      score = score + 400;
-      gotCorrect = -1;
-      correctChoice = 0;
-      mode = game; 
-    } else if(gotCorrect == 0){
-      pacman.x = 40;
-      pacman.y = 370;
-      lives = lives - 1;
-      gotCorrect = -1;
-      correctChoice = 0;
-      mode = game; 
-    } else{
-      mode = question;
-    }
-    
-  }
-
-  if(pacman.isTouching(p2)){
-    q1 = "qwduhn ekjbc wdcj"
-    a1 = "kwqacsjkefb"
-    a2 = "qwjckb"
-    a3 = "ecbecopqw"
-    correctChoice = 3;
-    
-    if(gotCorrect == 1){
-      p2.destroy();
-      score = score + 20;
-      gotCorrect = -1;
-      correctChoice = 0;
-    } else if(gotCorrect == 0){
-      pacman.x = 40;
-      pacman.y = 370;
-      lives = lives - 1;
-      gotCorrect = -1;
-      correctChoice = 0;
-    } else{
-      mode = question;
-    }
-  }
-  
-  if(pacman.isTouching(p3)){
-    p3.destroy();
-    score = score + 20;
-  }
-  
-  if(pacman.isTouching(p4)){
-    p4.destroy();
-    score = score + 20;
-  }
-  
-  if(pacman.isTouching(p5)){
-    p5.destroy();
-    score = score + 20;
-  }
-  
-  if(pacman.isTouching(p6)){
-    p6.destroy();
-    score = score + 20;
-  }
-
-  if(pacman.isTouching(orangeMonster)||pacman.isTouching(blueMonster)||pacman.isTouching(orangeMonster2)||pacman.isTouching(pinkMonster)||pacman.isTouching(redMonster)){
-    lives = lives - 1;
-    pacman.x = 40;
-    pacman.y = 370;
-    
-    if(lives === 0){
-      textSize(30);
-      fill("black");
-      text("GAME OVER",300,200);
-      blueMonster.velocityX = 0;
-      orangeMonster.velocityX = 0;
-      pinkMonster.velocityX = 0;
-      redMonster.velocityX = 0;
-      orangeMonster2.velocityX = 0; 
-      pacman.destroy();
-      
-      
-    }
-
-  }
-  
-  
 }
 
 function startGame() {
   mode += 1;
+  startButton.remove();
 }
 
 function questionMode() {
   square(50, 50, 300)
   fill('black');
-  textSize(20);
+  textSize(14);
   textAlign(CENTER, TOP);
-  text(q1, 200, 60);
+  text(q1, 50, 60, 300, 90);
   
   textSize(15);
   square(optionButtonX, 150, optionButtonWidth);
   textAlign(LEFT, TOP);
-  text(a1, 100, 150);
+  text(a1, 100, 150, 300, 90);
   
   square(optionButtonX, 190, optionButtonWidth);
-  text(a2, 100, 190);
+  text(a2, 100, 190, 300, 90);
   
   square(optionButtonX, 230, optionButtonWidth);
-  text(a3, 100, 230);
+  text(a3, 100, 230, 300, 90);
   
 }
 
@@ -335,4 +467,12 @@ function mouseClicked() {
     }
   }
   
+}
+
+function endMode(){
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  text("Game Over", 200, 200);
+  textSize(16);
+  text("Score: " + score, 200, 230);
 }
